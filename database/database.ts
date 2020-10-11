04c08b0f912298f549ce7e0816806b028b682031
Brett Fuller <brettfuller@bretts-mbp.lan>
2020-10-11 00:01:06 -0500

    addTests add item tests

--- database/database.ts
+++ database/database.ts
@@ -1,13 +1,20 @@
 // access environment variables
+let setupSchema;
 import dotenv from 'dotenv';
 dotenv.config();
 
-const getDBConnection = async () => {
+const getDBConnection = async (setupTestDB: boolean = false) => {
     const db = require('knex')({
         client: 'pg',
         connection: process.env.CONNECTION_STRING
     });
-    const setupSchema = require(`./scripts/db_schema`);
+
+    if (setupTestDB) {
+        setupSchema = require('./scripts/test_db_schema');
+    }
+    else {
+        setupSchema = require(`./scripts/db_schema`);
+    }
 
     await setupSchema(db);
 
--- tests/unit/items.test.js
+++ tests/unit/items.test.js
@@ -1,26 +1,24 @@
 const itemRepo = require('../../database/repositories/item');
-// create item test
 
-const item = { name: 'RobotTest101', cost: 10000, userId: 1};
+const _ACCOUNT = { id: 10, username: 'MrRoboto', email: '', active: true }
+const _ITEM = { name: 'RobotTest101', cost: 10000, accountId: 10};
 
-test('add item', async () => {
-    //create and add item
-    expect.assertions(1);
-    const resultingItem = await itemRepo.createItem(item);
-    expect(resultingItem.name).toMatch(item.name);
+describe('Item tests', () => {
+    it('add item to item table', async () => {
+        //create and add item
+        expect.assertions(1);
+        const resultingItem = await itemRepo.createItem(_item);
+        expect(resultingItem.name).toMatch(_ITEM.name);
+    });
+
+    it('find account items by accountID', async () => {
+        const results = await itemRepo.findAll(_ACCOUNT.id);
+        expect.assertions(2);
+        expect(results.length).toBeGreaterThan(0)
+    });
+
+    it('find item by itemId', async () => {
+        const result = await itemRepo.findById(_ITEM.id);
+    })
 });
-
-test('find items', async () => {
-    const results = await itemRepo.findAll(item.userId);
-    
-    const mappedToNames = results.map(i => i.name);
-
-    expect.assertions(2);
-    expect(results.length).toBeGreaterThan(0);
-    expect(mappedToNames).toContain(item.name);
-
-    // clean up db
-    // await itemRepo.deleteItem(item.userId, item.id);
-})
-
 
